75b61caa3947b4a4a510c00cffff9c2d7c5d955a
Miaoer Jin <miaoer@Miaoers-MacBook-Pro.local>
2021-04-08 17:44:25 -0700

    create forms for user admin

--- jin.mia/demo/users_admin.php
+++ jin.mia/demo/users_admin.php
@@ -25,7 +25,7 @@ echo <<<HTML
    </ul>
 </nav>
 <div>
-   <h2>$user->name</h2>
+   <h2>$user->fullname</h2>
    <div>
       <strong>Type</strong>
       <span>$user->type</span>
@@ -39,6 +39,69 @@ echo <<<HTML
       <span>$classes</span>
    </div>
 </div>
+
+<div class="grid gap">
+          <div class="col-xs-12 col-md-6">
+            <div class="form-control">
+            <label for="example1" class="form-label">First Name</label>
+            <input id="example1" type="text" placeholder="$user->firtsname" class="form-input">
+            </div>
+          </div>
+         <div class="col-xs-12 col-md-6">
+          <div class="form-control">
+            <label for="example2" class="form-label">Last Name</label>
+            <input id="example2" type="text" placeholder="$user->lastname" class="form-input">
+          </div>
+         </div>
+       </div>
+         
+         <div class="grid gap">
+          <div class="col-xs-12 col-md-6">
+           <div class="form-control">
+            <label for="example3" class="form-label">Address</label>
+            <input id="example3" type="text" placeholder="Address" class="form-input">
+            </div>
+          </div>
+         <div class="col-xs-12 col-md-6">
+          <div class="form-control">
+            <label for="example3" class="form-label">City</label>
+            <input id="example3" type="text" placeholder="City" class="form-input">
+          </div>
+        </div>
+      </div>
+
+      <div class="grid gap">
+          <div class="col-xs-12 col-md-6">
+           <div class="form-control">
+            <label for="example3" class="form-label">State</label>
+            <input id="example3" type="text" placeholder="State" class="form-input">
+            </div>
+          </div>
+         <div class="col-xs-12 col-md-6">
+          <div class="form-control">
+            <label for="example3" class="form-label">ZIP Code</label>
+            <input id="example3" type="text" placeholder="ZIP Code" class="form-input">
+          </div>
+        </div>
+      </div>
+
+       <div class="grid gap">
+          <div class="col-xs-12 col-md-6">
+           <div class="form-control">
+            <label for="example3" class="form-label">Email</label>
+            <input id="example3" type="text" placeholder="$user->email" class="form-input">
+            </div>
+          </div>
+         <div class="col-xs-12 col-md-6">
+          <div class="form-control">
+            <label for="example3" class="form-label">Phone Number</label>
+            <input id="example3" type="text" placeholder="Phone Number" class="form-input">
+          </div>
+        </div>
+      </div>
+      
+       <div class="center" style="margin-top: 4em"><a href="#" class="darkbutton">Submit</a></div>
+
 HTML;
 }
 
@@ -70,6 +133,8 @@ HTML;
    <div class="container">
       <div class="card soft">
 
+
+
          <?php
          if(isset($_GET['id'])) {
             showUserPage($users[$_GET['id']]);
@@ -83,7 +148,7 @@ HTML;
 
          for($i=0; $i<count($users); $i++) {
             echo "<li>
-            <a href='{$_SERVER['PHP_SELF']}?id=$i'>{$users[$i]->name}</a>
+            <a href='{$_SERVER['PHP_SELF']}?id=$i'>{$users[$i]->fullname}</a>
             </li>";
          }
 
