5b17fa4ae4d9af8552036f4cee51fc2413c1949f
Miaoer Jin <miaoer@Miaoers-MacBook-Pro.local>
2021-04-20 00:12:32 -0700

    add tab icon

--- jin.mia/parts/meta.php
+++ jin.mia/parts/meta.php
@@ -1,10 +1,13 @@
+  
+  <title>Litte Green House</title>
   <base href="/aau/wnm608/jin.mia/">
   
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
    
+  <link rel="icon" type = "image/png" href="img/tab.png">
   <link rel="stylesheet" href="lib/css/styleguide.css">
-   <link rel="stylesheet" href="lib/css/gridsystem.css">
+  <link rel="stylesheet" href="lib/css/gridsystem.css">
   <link rel="stylesheet" href="css/storetheme.css">
 
   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
